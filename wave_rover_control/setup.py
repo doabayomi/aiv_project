9d570de015fae184b0015efd07690bd15c0b2a7c
Winonymous <anjuwonololade@gmail.com>
2024-07-01 00:31:20 +0100

    Finalizing xacro file

--- wave_rover_control/setup.py
+++ wave_rover_control/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'wave_rover_control'
 
@@ -10,6 +12,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # (os.path.join('share', package_name, 'config'), glob('config/*')),
+        # (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        # (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
