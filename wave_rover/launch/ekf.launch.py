bc6e5b03d2872e53454c9cbcae335fe84c7b845e
Winonymous <anjuwonololade@gmail.com>
2024-07-24 00:43:07 +0100

    EKF fusing

--- wave_rover/launch/ekf.launch.py
+++ wave_rover/launch/ekf.launch.py
@@ -1,35 +1,43 @@
-# Copyright 2018 Open Source Robotics Foundation, Inc.
-# Copyright 2019 Samsung Research America
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from launch import LaunchDescription
-from ament_index_python.packages import get_package_share_directory
-import launch_ros.actions
 import os
-import yaml
-from launch.substitutions import EnvironmentVariable
-import pathlib
-import launch.actions
-from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+
+from launch.actions import GroupAction, TimerAction, DeclareLaunchArgument
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+
 
 def generate_launch_description():
-    return LaunchDescription([
-        launch_ros.actions.Node(
+    package_name='wave_rover' #<--- CHANGE ME
+    share_dir = get_package_share_directory(package_name)
+
+    pub_tranform = LaunchConfiguration('publish_transform')
+
+
+    lidar_odom_node = Node(
+        package="lidar_odometry",
+        executable="lidar_odometry_node"
+    )
+
+    ekf_node = Node(
             package='robot_localization',
             executable='ekf_node',
             name='ekf_filter_node',
             output='screen',
-            parameters=[os.path.join(get_package_share_directory("wave_rover_control"), 'params', 'ekf.yaml')],
-           ),
-])
+            parameters=[os.path.join(share_dir, 'params', 'imu_ekf.yaml')],
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "publish_transform",
+                default_value = "true",
+                description= "Publish transformation"
+            ),
+            lidar_odom_node, 
+            TimerAction(
+            actions=[ekf_node],
+            period =2.0)
+        ]
+    )
